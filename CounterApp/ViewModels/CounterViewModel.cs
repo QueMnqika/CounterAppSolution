972386f6df8cdf66a62fca05ab549c975ef48583
Que Mnqika <qaqambilemnqika23@gmail.com>
2025-08-08 13:21:02 +0200

    done with everything

--- CounterApp/ViewModels/CounterViewModel.cs
+++ CounterApp/ViewModels/CounterViewModel.cs
@@ -1,13 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace CounterApp.ViewModels
 {
-    public class CounterViewModel
+    public class CounterViewModel : INotifyPropertyChanged
     {
+        private int _count = 0;
+        private string _message = "Click button to start count";
 
-    }
+        public CounterViewModel()
+        {
+            IncrementCommand = new Command(Increment);
+            ResetCommand = new Command(Reset, CanReset);
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                OnPropertyChanged();
+
+                // Notify that CanExecute may have changed
+                ((Command)ResetCommand).ChangeCanExecute();
+            }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+            }
+        }
+
+        public ICommand IncrementCommand { get; }
+        public ICommand ResetCommand { get; }
+
+        public void Increment()
+        {
+            Count++;
+        }
+
+        public void Reset()
+        {
+            Count = 0;
+        }
+
+        public bool CanReset()
+        {
+            return Count > 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    } 
 }
+
